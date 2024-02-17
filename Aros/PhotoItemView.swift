b784b8484bd1d45e5689f6968b90b9b62f0f841f
Siddhant Sharma <sharmasiddhant301@gmail.com>
2024-02-16 23:09:40 -0800

    trying

---
+++ Aros/PhotoItemView.swift
@@ -0,0 +1,43 @@
+//
+//  PhotoItemView.swift
+//  Aros
+//
+//  Created by Siddhant Sharma on 2/16/24.
+//
+
+import SwiftUI
+import Photos
+
+struct PhotoItemView: View {
+    var asset: PhotoAsset
+    var cache: CachedImageManager?
+    var imageSize: CGSize
+    
+    @State private var image: Image?
+    @State private var imageRequestID: PHImageRequestID?
+
+    var body: some View {
+        
+        Group {
+            if let image = image {
+                image
+                    .resizable()
+                    .scaledToFill()
+            } else {
+                ProgressView()
+                    .scaleEffect(0.5)
+            }
+        }
+        .task {
+            guard image == nil, let cache = cache else { return }
+            imageRequestID = await cache.requestImage(for: asset, targetSize: imageSize) { result in
+                Task {
+                    if let result = result {
+                        self.image = result.image
+                    }
+                }
+            }
+        }
+    }
+}
+
