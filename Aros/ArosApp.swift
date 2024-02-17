fb90d1e3bc897054fc9e6b208535d87477340d0e
Maanav Khaitan <maanav@berkeley.edu>
2024-02-16 21:16:23 -0800

    Boilerplatew

---
+++ Aros/ArosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArosApp.swift
+//  Aros
+//
+//  Created by Maanav Khaitan on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct ArosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aros/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Aros
+//
+//  Created by Maanav Khaitan on 2/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
