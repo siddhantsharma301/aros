516aac4a92be83144acb40e8c26a545124bf0649
Anjan Bharadwaj <anjanb@berkeley.edu>
2024-02-17 17:14:22 -0800

    push to pk registry

---
+++ Aros/PublicKey.swift
@@ -0,0 +1,13 @@
+//
+//  PublicKey.swift
+//  Aros
+//
+//  Created by Anjan Bharadwaj on 2/17/24.
+//
+
+import Foundation
+// PublicKey.swift
+struct PublicKey: Codable {
+    var userId: String
+    var pubKey: String
+}
